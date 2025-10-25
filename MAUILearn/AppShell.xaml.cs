ec5aa5fd3897d6ceda270a693979cef002a85d9f
bruno sarzano <brunosarzanodias@gmail.com>
2025-10-24 21:20:29 -0300

    commit initial

---
+++ MAUILearn/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MAUILearn;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MAUILearn/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MAUILearn")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MAUILearn.Pages")]
